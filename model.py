5227fb5ec1dc46f5f5b7f5b2530b8b66d31bb8b8
Vedansh Shrivastava <64207178+Vedansh-Shrivastava@users.noreply.github.com>
2026-08-20 12:25:45 +0530

    Step 2: build_stoi

--- model.py
+++ model.py
@@ -22,8 +22,15 @@ def build_vocab(text):
 
     return vocab
 
-# Step 2 - build_stoi (not yet solved)
-# TODO: implement
+# Step 2 - build_stoi
+def build_stoi(vocab):
+    """Return a dict mapping each character in vocab to its index."""
+    # TODO: map each character in vocab to its integer position
+    stoi = {}
+    for i in range(len(vocab)):
+        stoi[vocab[i]] = i
+
+    return stoi
 
 # Step 3 - build_itos (not yet solved)
 # TODO: implement
